815f1cee21e8e04bdf4460cd2a348441cea54a94
Yogendra Singh <yog27ray@gmail.com>
2022-06-14 22:47:55 +0530

    build fix

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 import { CourierTracking, CourierTrackingExpress, Stage, Courier } from './src/courier-tracking';
+import { TrackingUpdate } from './typings/tracking-update';
 
-export { CourierTrackingExpress, CourierTracking, Stage, Courier };
+export { CourierTrackingExpress, CourierTracking, Stage, Courier, TrackingUpdate };
