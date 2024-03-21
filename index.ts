9624558a0d6d6f4b98abff93077acaee9e8fdf78
Yogendra Singh <yog27ray@gmail.com>
2024-03-21 18:16:53 +0530

    i

--- index.ts
+++
@@ -1,4 +0,0 @@
-import { Courier, CourierTracking, CourierTrackingExpress, Stage } from './src/courier-tracking';
-import { TrackingUpdate } from './typings/tracking-update';
-
-export { CourierTrackingExpress, CourierTracking, Stage, Courier, TrackingUpdate };
--- mocha.conf.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-
-// Load Chai assertions
-global.expect = chai.expect;
-global.assert = chai.assert;
-chai.should();
-
-// Initialize Chai plugins
-chai.use(require('chai-subset'));
